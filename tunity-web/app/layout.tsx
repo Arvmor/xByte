0c6bf1d1c44a2b10a8bc025408ff51ec47551ded
Arvin Morawej <arvin.morawej@gmail.com>
2025-11-26 23:58:44 -0500

    feat: Add Dark Theme

--- tunity-web/app/layout.tsx
+++ tunity-web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AppHeader from "@/components/app/header";
+import { AppTheme } from "@/components/app/theme";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AppHeader />
-        {children}
+        <AppTheme>
+          {children}
+        </AppTheme>
       </body>
     </html>
   );
