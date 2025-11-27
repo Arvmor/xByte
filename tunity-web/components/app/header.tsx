cb5b0918c9595b8fb7e3eb937b1da4322cf24b01
Arvin Morawej <arvin.morawej@gmail.com>
2025-11-27 01:36:23 -0500

    feat: Show User info privy

--- tunity-web/components/app/header.tsx
+++ tunity-web/components/app/header.tsx
@@ -2,27 +2,63 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Home } from "lucide-react";
-import { usePrivy } from "@privy-io/react-auth";
+import { Copy, Home } from "lucide-react";
+import { usePrivy, User } from "@privy-io/react-auth";
 
 /** The header component for the app */
 export default function AppHeader() {
-    const { connectOrCreateWallet } = usePrivy();
+    const { user, connectOrCreateWallet } = usePrivy();
 
     return (
         <div className="flex p-4 justify-between">
+            {/* Logo */}
             <h2 className="text-2xl font-bold">Tunity</h2>
 
+            {/* Search bar */}
             <div className="flex gap-2">
                 <Button variant="outline" size="icon"><Home /></Button>
                 <Input placeholder="What do you want to play?" />
             </div>
 
-            <div className="flex gap-2">
-                <Button variant="ghost">Sign up</Button>
-                <Button variant="outline" onClick={() => connectOrCreateWallet()}>Login</Button>
-            </div>
+            {/* User info*/}
+            {user ? <UserInfo user={user} /> : <Onboarding onClick={connectOrCreateWallet} />}
         </div>
     )
 }
 
+/** The onboarding component for the app */
+function Onboarding({ onClick }: { onClick: () => void }) {
+    return (
+        <div className="flex gap-2">
+            <Button variant="ghost" onClick={onClick}>Sign up</Button>
+            <Button variant="outline" onClick={onClick}>Login</Button>
+        </div>
+    )
+}
+
+/** The user info component for the app */
+function UserInfo({ user }: { user: User }) {
+    const address = user.wallet?.address;
+    const shortAddress = `${address?.slice(0, 8)}...${address?.slice(-8)}`;
+    const avatar = address?.[2] ?? "N/A";
+
+    /** Handle the copy of the address */
+    const handleCopy = () => {
+        if (!address) return;
+        navigator.clipboard.writeText(address);
+    }
+
+    return (
+        <div className="flex items-center gap-2">
+            {/* Address */}
+            <div className="flex bg-accent px-3 py-2 rounded-md items-center gap-2">
+                <Copy className="size-4" onClick={handleCopy} />
+                <p className="text-sm">{shortAddress}</p>
+            </div>
+            {/* Avatar */}
+            <div className="size-10 rounded-full bg-accent flex items-center justify-center">
+                {avatar}
+            </div>
+        </div>
+    )
+}
