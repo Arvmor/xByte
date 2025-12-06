5fed96f71812269210cd9a671f59a504d93b433e
Arvin Morawej <arvin.morawej@gmail.com>
2025-12-05 20:36:52 -0500

    feat: Add dynamic byte requrest endpoint

--- tunity-server/crates/tunity-api/src/player.rs
+++ tunity-server/crates/tunity-api/src/player.rs
@@ -1,8 +1,11 @@
 use crate::ResultAPI;
 use crate::x402::{ConfigX402, FacilitatorRequest, PaymentExtractor, PaymentRequest, X402Response};
+use actix_web::Responder;
 use actix_web::dev::HttpServiceFactory;
-use actix_web::{HttpRequest, web};
-use actix_web::{Responder, get};
+use actix_web::{HttpRequest, post, web};
+use serde::Deserialize;
+use std::io::{BufReader, Read, Seek, SeekFrom};
+use std::path::Path;
 
 /// The Player Routes
 #[derive(Debug)]
@@ -19,11 +22,23 @@ impl HttpServiceFactory for PlayerRoute {
     }
 }
 
+/// The request to play a sample
+#[derive(Debug, Deserialize)]
+pub struct PlayRequest {
+    /// The file to play
+    pub file: String,
+    /// The offset to start playing from
+    pub offset: u64,
+    /// The length of the sample to play
+    pub _length: usize,
+}
+
 /// The play endpoint
-#[get("/play")]
+#[post("/play")]
 async fn play(
     request: HttpRequest,
     config: web::Data<ConfigX402<&'static str>>,
+    payload: web::Json<PlayRequest>,
     auth: Option<PaymentExtractor>,
 ) -> impl Responder {
     let url = request.full_url();
@@ -39,32 +54,24 @@ async fn play(
     let facilitator = FacilitatorRequest::new(payment, request.accepts[0].clone());
     if let Ok(response) = facilitator.verify()
         && Some(true) == response.is_valid
-        && let Ok(audio_sample) = get_audio_sample()
+        && let Ok(audio_sample) = get_sample(&*payload.file, payload.offset)
     {
         actix_web::rt::spawn(async move { facilitator.settle() });
 
         // Get Audio Sample
-        return ResultAPI::verified_payment(audio_sample);
+        return ResultAPI::verified_payment(audio_sample.to_vec());
     };
 
     ResultAPI::payment_required(request)
 }
 
-fn get_audio_sample() -> anyhow::Result<Vec<u8>> {
-    const SAMPLE_DURATION_SECONDS: usize = 3;
-    const FRAMES_PER_SECOND: usize = 38;
-    const TOTAL_FRAMES: usize = SAMPLE_DURATION_SECONDS * FRAMES_PER_SECOND;
+fn get_sample<F: AsRef<Path>>(file: F, offset: u64) -> anyhow::Result<[u8; 300_000]> {
+    let file = std::fs::File::open(file)?;
+    let mut reader = BufReader::new(file);
 
-    let mut sample = Vec::new();
+    reader.seek(SeekFrom::Start(offset))?;
+    let mut buff = [0u8; 300_000];
+    reader.read_exact(&mut buff)?;
 
-    for _ in 0..TOTAL_FRAMES {
-        sample.extend_from_slice(&[0xFF, 0xFB, 0x90, 0x00]);
-
-        let frame_size: usize = 417;
-        for i in 0..frame_size.saturating_sub(4) {
-            sample.push((i % 256) as u8);
-        }
-    }
-
-    Ok(sample)
+    Ok(buff)
 }
