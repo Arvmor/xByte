aa00bbac536935c4cbd0bf2c727433f68c4d4f53
Arvin Moravej <moravej.arvin@gmail.com>
2025-12-24 18:59:48 -0500

    feat: Add optionable component

---
+++ xbyte-web/xbyte-platform/components/platform/optionable.tsx
@@ -0,0 +1,26 @@
+import { Building2 } from "lucide-react";
+
+export interface OptionableProps {
+    titleText: string;
+    descriptionText?: string;
+    Icon?: React.ElementType;
+    onClick?: () => void;
+}
+
+export default function Option({ titleText, descriptionText, Icon, onClick }: OptionableProps) {
+    return (
+        <div
+            className="flex flex-col items-center bg-muted rounded-sm p-8 h-100 w-full cursor-pointer hover:border-muted-foreground border-transparent border-3 transition-all duration-300"
+            onClick={onClick}
+        >
+            <div className="flex items-center justify-center h-full text-background">
+                {Icon && <Icon className="size-24" />}
+            </div>
+
+            <div className="flex flex-col items-center justify-center text-center">
+                <h1 className="text-sm text-muted-foreground">{titleText}</h1>
+                <h2 className="text-xl font-bold">{descriptionText}</h2>
+            </div>
+        </div>
+    );
+}
