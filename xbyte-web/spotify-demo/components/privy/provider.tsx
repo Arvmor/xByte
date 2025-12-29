ff4b6ee742dc55f84ad35265c5d4c827c5d57a2a
Arvin Moravej <moravej.arvin@gmail.com>
2025-12-29 14:58:51 -0500

    refacotr: Privy Client component

--- xbyte-web/spotify-demo/components/privy/provider.tsx
+++ xbyte-web/spotify-demo/components/privy/provider.tsx
@@ -1,24 +1,20 @@
 "use client";
 
-import { PrivyProvider } from "@privy-io/react-auth";
+import { PrivyClientConfig, PrivyProvider } from "@privy-io/react-auth";
 
 const PRIVY_APP_ID = "cmigyzusl00qhl20cl7qtnczl";
 const PRIVY_CLIENT_ID = "client-WY6TMNBR45a7ie2SwSpVPK8vHJFEsGvHnPf7Ph2n4LBnN";
+const config: PrivyClientConfig = {
+    embeddedWallets: {
+        ethereum: {
+            createOnLogin: "users-without-wallets",
+        },
+    },
+};
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <PrivyProvider
-            appId={PRIVY_APP_ID}
-            clientId={PRIVY_CLIENT_ID}
-            config={{
-                // Create embedded wallets for users who don't have a wallet
-                embeddedWallets: {
-                    ethereum: {
-                        createOnLogin: "users-without-wallets",
-                    },
-                },
-            }}
-        >
+        <PrivyProvider appId={PRIVY_APP_ID} clientId={PRIVY_CLIENT_ID} config={config}>
             {children}
         </PrivyProvider>
     );
